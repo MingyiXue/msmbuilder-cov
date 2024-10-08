3f01d2e8b3f7e4c8ae0c14be5e86ff00192df02f
Mingyi Xue <mxue33@wisc.edu>
2024-10-08 12:08:12 -0500

    upload code and action

---
+++ msmbuilder/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '3.9.0'
+short_version = '3.9'
+full_version = '3.9.0+4b0015c24062befc96d1ddd52c81d0a4f6ef264d'
+git_revision = '4b0015c24062befc96d1ddd52c81d0a4f6ef264d'
+release = False
